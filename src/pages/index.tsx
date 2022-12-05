6801d201c8a6735329a303dbc0766a54f4501db1
Alicia Arver <alessarver@gmail.com>
2022-12-05 20:42:29 +0300

    fix redirect at index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,19 @@
 import { useReactiveVar } from "@apollo/client";
 
-import { meLoadingVar, meVar } from "apollo/variables/user";
+import { meVar } from "apollo/variables/user";
 import Router from "next/router";
 import { useEffect } from "react";
 
 export default function Home() {
   const me = useReactiveVar(meVar);
-  const meLoading = useReactiveVar(meLoadingVar);
 
   useEffect(() => {
-    if (!meLoading) {
-      if (me) {
-        Router.push("/messages");
-      } else {
-        Router.push("/login");
-      }
+    if (me) {
+      Router.push("/messages");
+    } else {
+      Router.push("/login");
     }
-  }, [me, meLoading]);
+  }, [me]);
 
   return <div></div>;
 }
